b8db295075ec7dfae7343883e3b6ba5229bd675c
Vince Speelman <vincespeelman@quickenloans.com>
2013-07-02 16:24:37 -0400

    add style compile

---
+++ config.rb
@@ -0,0 +1,16 @@
+require 'modular-scale'
+require 'susy'
+require 'omg-text'
+
+sass_dir = 'src/styles'
+css_dir = 'styles'
+images_dir = 'images'
+javascripts_dir = 'scripts'
+fonts_dir = 'styles/fonts'
+import_path = 'components'
+http_images_path = '/images'
+http_generated_images_path = '/images/generated'
+http_fonts_path = '/styles/fonts'
+relative_assets = false
+output_style = :expanded
+preferred_syntax = :sass
